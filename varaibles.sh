06e378a448e99cd72e519ed180362254a2256c1a
Hemanth Ponugothi <phemanth8888@gmail.com>
2025-12-06 17:24:40 +0100

    adding var

---
+++ varaibles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+person1=hemanth
+
+person2=mahesh
+
+echo "$person1 :: What is Shell Scripting?"
+
+echo "$person2 ::scripts (programs you write) to automate tasks like: 
+checking AWS resources, 
+scanning logs, 
+monitoring servers, 
+doing security audits"
+
+echo "$person1 :: what are advantage of using shell scripting "
+
+echo "$person2 :: 
+1. Speed
+2. Accuracy, no human errors
+3. Consistency
+4. Scalability, can work for infinite number of servers
+5. Documentaion. What happened including history
+6. Reusability. "
+
