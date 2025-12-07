0a328192185fa4573fad54c2f4f6ff592bf501c3
Hemanth Ponugothi <phemanth8888@gmail.com>
2025-12-07 20:37:56 +0100

    Adding Extra Details

---
+++ 09-installation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Automated Installation of pacakges
+
+USERID=$(id -u)
+if [ "$USERID" -ne 0 ]; then
+    echo "THis script must be run as root or with sudo."
+    exit 6
+fi
+
+dng install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "MySQL Installation failure."
+    exit 5
+else
+    echo "MySQL installed successfully."
+fi
