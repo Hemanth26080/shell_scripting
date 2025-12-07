9821ad988f8d6aad8817979a6ff38a9866b5785b
Hemanth Ponugothi <phemanth8888@gmail.com>
2025-12-07 22:38:24 +0100

    Adding Extra Details

--- 11-logs.sh
+++ 11-logs.sh
@@ -11,10 +11,10 @@ N="\e[0m"
 # Create log directory and log file
 mkdir -p /var/log/shellscripts
 LOGFolder="/var/log/shellscripts"
-SCRIPT_NAME=$( echo $0 | cut -d '.' -f1 ) # 11-logs
+SCRIPT_NAME= $( basename "$0" .sh )      #$( echo $0 | cut -d '.' -f1 ) # 11-logs
 LOG_FILE="$LOGFolder"/$SCRIPT_NAME.log # /var/log/shell-script/11-logs.log
 
-echo "Script Execution started at: $(date)" &>> "$LOG_FILE" | tee -a "$LOG_FILE"
+echo "Script Execution started at: $(date)" | tee -a "$LOG_FILE"
 
 if [ $USERID -ne 0 ]; then
     echo -e "ERROR:: Please run this script with root privelege" | tee -a "$LOG_FILE"
@@ -34,27 +34,27 @@ VALIDATE() {
 }
 
 # Check and Install Mysql
-dnf list installed mysql &>> "$LOG_FILE" | tee -a "$LOG_FILE"
+dnf list installed mysql &>> "$LOG_FILE" 
 if [ $? -ne 0 ]; then
-    dnf install mysql -y &>> "$LOG_FILE" | tee -a "$LOG_FILE"
+    dnf install mysql -y &>> "$LOG_FILE" 
     VALIDATE $? "Mysql"
 else
     echo -e "Mysql already exits ... $Y SKIPPING $N" | tee -a "$LOG_FILE"
 fi
 
 # Check and Install Nginx
-dnf list installed nginx &>> "$LOG_FILE" | tee -a "$LOG_FILE"
+dnf list installed nginx &>> "$LOG_FILE" 
 if [ $? -ne 0 ]; then
-    dnf install nginx -y &>> "$LOG_FILE" | tee -a "$LOG_FILE"
+    dnf install nginx -y &>> "$LOG_FILE" 
     VALIDATE $? "Nginx"
 else
     echo -e "Nginx already exits ... $Y SKIPPING $N" | tee -a "$LOG_FILE"
 fi
 
 # Check and Install Python3
-dnf list installed python3 &>> "$LOG_FILE" | tee -a "$LOG_FILE"
+dnf list installed python3 &>> "$LOG_FILE" 
 if [ $? -ne 0 ]; then
-    dnf install python3 -y &>> "$LOG_FILE" | tee -a "$LOG_FILE"
+    dnf install python3 -y &>> "$LOG_FILE" 
     VALIDATE $? "Python3"
 else
     echo -e "Python3 already exits ... $Y SKIPPING $N" | tee -a "$LOG_FILE"
