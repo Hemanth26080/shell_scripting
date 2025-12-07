9abfbf921f88712d929dfffb647be712e458bf4b
Hemanth Ponugothi <phemanth8888@gmail.com>
2025-12-07 21:06:38 +0100

    Adding Extra Details

--- 09-functiosn.sh
+++ 09-functiosn.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-set -euo pipefail
-
-# Automate the installation of multiple packages using a function
-
-validation() {
 USERID=$(id -u)
-    if [ "$USERID" -ne 0 ]; then
-        echo "This script must be run as root or with sudo."
-        exit 6
-    fi
-}
 
-install_pacakge() {
-    local package_name=$1
-    validation()
-    echo "installing $package_name"
-    dnf install "$package_name" -y
-    if [ $? -ne 0 ]; then
-        echo "$package_name installation failed."
-        exit 5
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installing $2 is failure"
+        exit 1
     else
-        echo "$package_name installed successfully."
+        echo "Installing $2 is SUCCESS"
     fi
 }
 
-pacakges_list=("mysql" "nginx" "python3" )
-for pacakges_name in "${pacakges_list[@]}"; do
-    install_pacakge "$pacakges_name"
-done
+dnf install mysql -y
+VALIDATE $? "MySQL"
 
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install python3 -y
+VALIDATE $? "python3"
