db3ad67968bad983830941d13cc236555897a284
Hemanth Ponugothi <phemanth8888@gmail.com>
2025-12-07 20:27:01 +0100

    Adding Extra Details

--- 06-if_test.sh
+++ 06-if_test.sh
@@ -10,3 +10,28 @@ else
     echo "$num is odd number."
 fi
 
+#creating basic calculator using nested if statements
+echo "Welcome to the basic calculator!"
+echo "Enter first number:"
+read num1
+echo "Enter second number:"
+read num2
+echo "Choose an operation: +, -, *, /"
+read operation
+if [[ $operation == "+" ]]; then
+    result=$((num1 + num2))
+elif [[ $operation == "-" ]]; then
+    result=$((num1 - num2))
+elif [[ $operation == "*" ]]; then
+    result=$((num1 * num2))
+elif [[ $operation == "/" ]]; then
+    if (( num2 == 0 )); then
+        echo "Error: Division by zero is not allowed."
+        exit 1
+    fi
+    result=$((num1 / num2))
+else
+    echo "Invalid operation selected."
+    exit 1
+fi
+echo "The result of $num1 $operation $num2 is: $result"
