bc24d5f6d77c652d8ea3f15f41b61cb31e6ab404
Hemanth Ponugothi <phemanth8888@gmail.com>
2025-12-28 22:58:21 +0100

    shell-pratice

---
+++ 17-generate_password.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+LENGTH=$1
+
+if [ -z "$LENGTH" ]; then
+  echo "Usage: $0 <password-length>"
+  exit 1
+fi
+
+CHARS='A-Za-z0-9!@#$%^&*()_+='
+
+password=$(tr -dc "$CHARS" < /dev/urandom | head -c "$LENGTH")
+echo "Generated password: $password"
+
