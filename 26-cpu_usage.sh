2b38c8af9378617addb235d606dad023b9582a98
Hemanth Ponugothi <phemanth8888@gmail.com>
2026-01-11 10:47:20 +0100

    updated script

--- 26-cpu_usage.sh
+++ 26-cpu_usage.sh
@@ -6,10 +6,11 @@ CPU_THRESHOLD=5 # in project we keep it as 75
 echo "CPU Usage Alert Script Started"
 echo "Threshold set to ${CPU_THRESHOLD}%"
 
-CPU_IDLE=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}' | cut -d. -f1)
-CPU_USAGE=$((100 - $CPU_IDLE))
+CPU_IDLE=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}' | cut -d. -f1) || CPU_IDLE=0
+CPU_USAGE=$((100 - ${CPU_IDLE:-0}))
 
 echo "Current CPU Usage: ${CPU_USAGE}%"
+
 if [ "$CPU_USAGE" -ge "$CPU_THRESHOLD" ]; then
     echo "ALERT: CPU usage is HIGH (${CPU_USAGE}%)"
 else
