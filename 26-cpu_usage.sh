1996a002407920b03fe645235d8b054628e0fe54
Hemanth Ponugothi <phemanth8888@gmail.com>
2026-01-11 10:41:20 +0100

    updated script

--- 26-cpu_usage.sh
+++ 26-cpu_usage.sh
@@ -7,7 +7,6 @@ echo "CPU Usage Alert Script Started"
 echo "Threshold set to ${CPU_THRESHOLD}%"
 
 CPU_IDLE=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}' | cut -d. -f1)
-CPU_USAGE=$((100 - CPU_IDLE))
+CPU_USAGE=$((100 - $CPU_IDLE))
 
 echo "Current CPU Usage: ${CPU_USAGE}%"
-
