d9fd09e1a3b795d4ddb83a741e724f3734c03be3
Hemanth Ponugothi <phemanth8888@gmail.com>
2026-01-11 10:43:32 +0100

    updated script

--- 26-cpu_usage.sh
+++ 26-cpu_usage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-CPU_THRESHOLD=80
+CPU_THRESHOLD=5 # in project we keep it as 75
 
 echo "CPU Usage Alert Script Started"
 echo "Threshold set to ${CPU_THRESHOLD}%"
@@ -10,3 +10,9 @@ CPU_IDLE=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}' | cut -d. -f1)
 CPU_USAGE=$((100 - $CPU_IDLE))
 
 echo "Current CPU Usage: ${CPU_USAGE}%"
+if [ "$CPU_USAGE" -ge "$CPU_THRESHOLD" ]; then
+    echo "ALERT: CPU usage is HIGH (${CPU_USAGE}%)"
+else
+    echo "OK: CPU usage is under control (${CPU_USAGE}%)"
+fi
+
